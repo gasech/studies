f34231899a18e54da106a65eb911ff41628df352
Gabriel <80397001+gasech@users.noreply.github.com>
2023-01-04 22:18:31 -0300

    fix spaces in quick sort js

--- algorithms/quick-sort.js
+++ algorithms/quick-sort.js
@@ -1,17 +1,18 @@
-function QuickSort(arr){
-  if(arr.length < 2) return arr;
+function QuickSort(arr) {
+  if (arr.length < 2) return arr;
 
   let pivot = arr[arr.length - 1];
   let left = [];
   let right = [];
-  
-  for(let i = 0; i < arr.length - 1; i++){
-    if(arr[i] >= pivot) {
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] >= pivot) {
       right.push(arr[i]);
     } else {
       left.push(arr[i]);
     }
   }
+
   let sortedLeft = QuickSort(left);
   let sortedRight = QuickSort(right);
 
@@ -23,8 +24,8 @@ function TestSorting(callback) {
   let sortedNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   let sortedTest = callback(nums);
-  
-  if(JSON.stringify(sortedTest) == JSON.stringify(sortedNums)){
+
+  if (JSON.stringify(sortedTest) == JSON.stringify(sortedNums)) {
     console.log("Test passed!");
   } else {
     console.log("Test not passed! Output:");
